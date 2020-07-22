7290488b27f184f8d9862fe7a61f59c90d36b46b
NewdleCat <mrfluffy900>
2020-07-21 17:08:43 -0700

    Added vertical tree

--- brain.lua
+++ brain.lua
@@ -260,6 +260,8 @@ function DrawBT(rootNode)
 
                 if string.find(nodeName, "takeCover") then -- for some reason words love to get up close
                     xOffset = xOffset + 150                -- and personal with "takeCover"
+                elseif string.find(nodeName, "walky away from enemy") then 
+                    xOffset = xOffset + 150                
                 end
 
             end
